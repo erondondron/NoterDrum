f4297f7f9f9d19842e729c2b531c6524fa073f07
Andrey Smirnov <smirnovas@modeltech.ru>
2025-03-20 23:34:27 +0300

    Set up fonts

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 final ThemeData darkTheme = ThemeData(
   brightness: Brightness.dark,
   scaffoldBackgroundColor: const Color(0xFF1F1F1F),
+  fontFamily: GoogleFonts.baloo2().fontFamily,
   appBarTheme: const AppBarTheme(
     backgroundColor: Color(0xFF1F1F1F),
   ),
